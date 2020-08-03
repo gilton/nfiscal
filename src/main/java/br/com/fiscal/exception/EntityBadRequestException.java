9de433043162f56a2b065d4ac57d6cf2699d472e
gilton <gilton_nasc@yahoo.com.br>
2020-08-03 16:01:32 -0300

    chore: changing the name EmpresaBadRequestException to EntityBadRequestException

--- src/main/java/br/com/fiscal/exception/EntityBadRequestException.java
+++ src/main/java/br/com/fiscal/exception/EntityBadRequestException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class EmpresaBadRequestException extends RuntimeException {
+public class EntityBadRequestException extends RuntimeException {
 	
-	public EmpresaBadRequestException() {
+	public EntityBadRequestException() {
 		super("Argumento de buscar inválido!");
 	}
 	
